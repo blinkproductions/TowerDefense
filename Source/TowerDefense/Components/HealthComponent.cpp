7573617e208efafbd3bfc2009027de362db26378
oO-Blink-Oo <alvarezian23@gmail.com>
2021-10-04 03:54:57 -0700

    Implemented HealthComponent
    HealthComponent is an actor component that handles regaining and losing armor and health

---
+++ Source/TowerDefense/Components/HealthComponent.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	CurrentArmor = MaxArmor;
+	CurrentHealth = MaxHealth / 2;
+	bIsDead = false;
+}
+
+void UHealthComponent::TakeDamage(float AmountOfDamage)
+{
+	if (CurrentArmor > 0)
+	{
+		CurrentArmor -= AmountOfDamage;
+	} else if (CurrentHealth > 0)
+	{
+		CurrentHealth -= AmountOfDamage;	
+	} else if (CurrentHealth <= 0)
+	{
+		bIsDead = true;
+	}
+}
+
+void UHealthComponent::Heal(float HealAmount)
+{
+	CurrentHealth += HealAmount;
+}
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/TowerDefense/Components/HealthComponent.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TOWERDEFENSE_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+		
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+	// FUNCTIONS
+	UFUNCTION(BlueprintCallable)
+	void TakeDamage(float AmountOfDamage);
+
+	UFUNCTION(BlueprintCallable)
+	void Heal(float HealAmount);
+
+	void SetHealth(float NewHealthAmount) { CurrentHealth = NewHealthAmount; }
+	void SetArmor(float NewArmorAmount) { CurrentArmor = NewArmorAmount; }
+
+	UFUNCTION(BlueprintPure)
+	float GetCurrentHealthPercentage() const { return CurrentHealth / 100.f; }
+
+	UFUNCTION(BlueprintPure)
+	float GetCurrentArmorPercentage() const { return CurrentArmor / 100.f; }
+	
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool bIsDead;
+	
+private:
+	// VARIABLES
+	float CurrentHealth;
+	float CurrentArmor;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Health Properties")
+	float MaxHealth = 100.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Health Properties")
+	float MaxArmor = 50.f;
+};
